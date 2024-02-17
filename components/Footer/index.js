e8dd42c1fefe0a286da94ebf2a3687d3ba924441
danielamansolino <danielamosqueragranda@gmail.com>
2024-02-17 16:46:52 -0500

    Blog Editor update

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            {/* <Button type="primary">Schedule a call</Button> */}
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,7 +24,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{""}
+        Made With ❤ by {""}
         <Link href="https://www.linkedin.com/in/daniela-mansolino-5b2608143/">
           <a className="underline underline-offset-1">Daniela Mansolino</a>
         </Link>
